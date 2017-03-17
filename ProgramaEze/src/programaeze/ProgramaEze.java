4d3e2ab6dcb7ec7d1030cd1731b972ba733a1ec9
Profesor <Profesor@192.168.1.55>
2017-03-17 11:51:03 +0100

    Primer commit...

---
+++ ProgramaEze/src/programaeze/ProgramaEze.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programaeze;
+
+/**
+ *
+ * @author Profesor
+ */
+public class ProgramaEze {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float porcentaje;
+        porcentaje=(12/16);
+        System.out.println("El porcentaje de alumn@s que quieren que gane el Barça es el "+porcentaje+" %");
+    }
+    
+}
