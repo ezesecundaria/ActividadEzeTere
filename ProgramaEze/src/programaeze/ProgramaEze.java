efa6fab235e0ff5fac703539cd3ae23b4bcdb3e7
Profesor <Profesor@192.168.1.55>
2017-03-17 12:37:10 +0100

    Primer commit... Modificando y poniendo el porcentaje a 80%... Acabo de camtiar a 75 el porcentaje para que no me deje subirlo pq mi versión local es diferente a la remota (he modificado la remota manualmente) Vuelvo a modificar el porcentaje a 70% desde el local y lo subo... Este commit es en la rama IntentoEstadistica que hemos creado para ver cómo funciona esto de las ramas...

--- ProgramaEze/src/programaeze/ProgramaEze.java
+++ ProgramaEze/src/programaeze/ProgramaEze.java
@@ -20,6 +20,9 @@ public class ProgramaEze {
         porcentaje= 70;
         System.out.println("El porcentaje de alumn@s que quieren que gane el Barça es el "+porcentaje+" %");
         System.out.println("Este cambio se ha hecho en el repositorio remoto...");
+        System.out.println("Really?");
+        System.out.println("Estoy trabajando en otra rama llamada IntentoEstadistica");
+        System.out.println("Si esta rama funciona, luego intentaré fusionarla con la otra...");
     }
     
 }
